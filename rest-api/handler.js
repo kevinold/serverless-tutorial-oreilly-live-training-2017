e19c65e775bae26506b7ddfdb5308d45aa780d46
Kevin Old <kevin@kevinold.com>
2017-09-06 21:17:15 -0500

    fix typo

--- rest-api/handler.js
+++ rest-api/handler.js
@@ -4,7 +4,7 @@ const db = require("./utils/dynamoUtils");
 
 module.exports = {
   create: (event, context, cb) => db.createJoke(event, cb),
-  list: (event, context, cb) => db.listJoke(event, cb),
+  list: (event, context, cb) => db.listJokes(event, cb),
   get: (event, context, cb) => db.getJoke(event, cb),
   update: (event, context, cb) => db.updateJoke(event, cb),
   delete: (event, context, cb) => db.deleteJoke(event, cb)
